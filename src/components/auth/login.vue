a4642a751e311ad99430f57ebcc7153ac5221cf2
Juar <juarsajolan64@gmail.com>
2026-03-11 16:42:38 +0800

    added a basic form of login

--- src/components/auth/login.vue
+++ src/components/auth/login.vue
@@ -1,25 +1,90 @@
 <script setup lang="ts">
 import { z } from 'zod'
+import { useForm } from '@tanstack/vue-form'
+import { Field, FieldContent, FieldError, FieldGroup, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const formSchema = z.object({
   username: z.string(),
-  password: z.string()
-  }
-)
+  password: z.string(),
+})
 
-fetch('http://localhost:8000/user', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
+const form = useForm({
+  validators: {
+    onSubmit: formSchema,
   },
-  body: JSON.stringify({full_name: 'Testificate test', password:'test', email:'test@test.com'}),
-}).then(res => res.json()).then(data => console.log(data))
+  onSubmit: async ({ value }) => {
+    const formData = new FormData()
 
+    for (const key in value) {
+      formData.append(key, value[key])
+    }
+
+    fetch('http://localhost:8000/auth', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  },
+})
+
+function isInvalid(field: any) {
+  return field.state.meta.isTouched && !field.state.meta.isValid
+}
 </script>
 
 <template>
+  <form id="form-login" @submit.prevent="form.handleSubmit">
+    <FieldGroup>
+      <form.Field name="username">
+        <template #default="{ field }">
+          <Field :data-invalid="isInvalid(field)">
+            <FieldContent>
+              <FieldLabel :for="field.name"> Email </FieldLabel>
+              <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+            </FieldContent>
+            <Input
+              :id="field.name"
+              :name="field.name"
+              :model-value="field.state.value"
+              :aria-invalid="isInvalid(field)"
+              placeholder="Email"
+              type="email"
+              @blur="field.handleBlur"
+              @input="field.handleChange($event.target.value)"
+            />
+          </Field>
+        </template>
+      </form.Field>
+    </FieldGroup>
+
+    <FieldGroup>
+      <form.Field name="password">
+        <template #default="{ field }">
+          <Field :data-invalid="isInvalid(field)">
+            <FieldContent>
+              <FieldLabel :for="field.name"> Password </FieldLabel>
+              <FieldError v-if="isInvalid(field)" :errors="field.state.meta.errors" />
+            </FieldContent>
+            <Input
+              :id="field.name"
+              :name="field.name"
+              :model-value="field.state.value"
+              :aria-invalid="isInvalid(field)"
+              placeholder="password"
+              type="password"
+              @blur="field.handleBlur"
+              @input="field.handleChange($event.target.value)"
+            />
+          </Field>
+        </template>
+      </form.Field>
+    </FieldGroup>
+
+    <Button type="submit">Submit</Button>
+  </form>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
